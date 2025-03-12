d40d92b64866e9fdfea476e45291fa69723bc4ba
Ezekiel Hamisu <mdhamisu1999@gmail.com>
2025-03-12 21:47:24 +0100

    initial commit

---
+++ src/main/java/com/genysyxtechnologies/service_request_system/ServiceRequestSystemApplication.java
@@ -0,0 +1,13 @@
+package com.genysyxtechnologies.service_request_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceRequestSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRequestSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/genysyxtechnologies/service_request_system/ServiceRequestSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genysyxtechnologies.service_request_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRequestSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

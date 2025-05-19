b9da0adeab57c693afbbd4fbe7e926565eeda2c2
Richard Danladi <richarddanladi7@gmail.com>
2025-05-19 12:37:50 +0100

    fixes

--- src/main/java/com/genysyxtechnologies/service_request_system/dtos/request/SubmitRequestDTO.java
+++ src/main/java/com/genysyxtechnologies/service_request_system/dtos/request/SubmitRequestDTO.java
@@ -1,8 +1,9 @@
 package com.genysyxtechnologies.service_request_system.dtos.request;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record SubmitRequestDTO(
-        String description,
+        @NotBlank(message = "Request data required") String requestData,
         @NotNull(message = "User department ID is required") Long userDepartmentId
 ) {}

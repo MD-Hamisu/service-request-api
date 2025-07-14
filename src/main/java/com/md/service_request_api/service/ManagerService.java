006288196a7bb75704936c9c048c7a49f79eb848
Ezekiel Hamisu <mdhamisu1999@gmail.com>
2025-07-14 16:34:22 +0100

    update 1

--- src/main/java/com/md/service_request_api/service/ManagerService.java
+++ src/main/java/com/md/service_request_api/service/ManagerService.java
@@ -1,15 +1,14 @@
 package com.md.service_request_api.service;
 
-import java.util.List;
-
-import com.md.service_request_api.dtos.request.UpdateStatusDto;
-import com.genysyxtechnologies.service_request_system.dtos.response.*;
 import com.md.service_request_api.constant.ServiceRequestStatus;
 import com.md.service_request_api.dtos.request.CategoryDTO;
 import com.md.service_request_api.dtos.request.ServiceOfferingDTO;
+import com.md.service_request_api.dtos.request.UpdateStatusDto;
 import com.md.service_request_api.dtos.response.*;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+
+import java.util.List;
 
 public interface ManagerService {
     DashboardResponse getDashboardStats();
